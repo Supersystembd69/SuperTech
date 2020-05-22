c5742b2bb2dcf32a78a4884bd9b6be1530107c20
Supersystembd69 <supersystembd69@yahoo.com>
2020-05-22 11:21:49 -0400

    1.2.3.4-----------

--- src/test/java/tests/SuperTechTest.java
+++ src/test/java/tests/SuperTechTest.java
@@ -15,9 +15,9 @@ public class SuperTechTest extends BaseClass{
 
 	@Test(priority=0)
 	public void superTech() throws InterruptedException {
-		test=report.createTest("SuperTect");
+		test=report.createTest("SuperTech");
 		SuperTechHomePage suhp=PageFactory.initElements(driver, SuperTechHomePage.class);
-		driver.get(prop.getProperty("file:///E:/WebSite/SuperTech.html"));
+		driver.get("file:///E:/WebSite/SuperTech.html");
 		suhp.mouseHover();
 		suhp.clickOnGoogle();
 		ArrayList<String>tab1= new ArrayList<String>(driver.getWindowHandles());
@@ -27,33 +27,27 @@ public class SuperTechTest extends BaseClass{
 	}
 	@Test(priority=1)
 	public void googleTest() throws InterruptedException {
-		test=report.createTest("SuperTect");
+		test=report.createTest("SuperTech");
 		GooglePage gop=PageFactory.initElements(driver, GooglePage.class);
 		gop.enterInsearchBox("Bangladesh");
 		Thread.sleep(4000);
-		ArrayList<String>tab0= new ArrayList<String>(driver.getWindowHandles());
-		driver.switchTo().window(tab0.get(0));
-		
-		Thread.sleep(4000);		
+			
 	}
 
 	@Test(priority=2)
 	public void iframeTest() throws InterruptedException {
+		
 		test=report.createTest("SuperTech");
 		SuperTechHomePage suhp=PageFactory.initElements(driver, SuperTechHomePage.class);
+		
+		ArrayList<String>tab0= new ArrayList<String>(driver.getWindowHandles());
+		driver.switchTo().window(tab0.get(0));
 		
 		suhp.enterOnIframeFname("Ataur");
 		Thread.sleep(4000);
 	}
-	@Test(priority=2)
-	public void enterFirstName() throws InterruptedException {
-		test=report.createTest("SuperTech");
-		SuperTechHomePage suhp=PageFactory.initElements(driver, SuperTechHomePage.class);
-		
-		suhp.enterOnIframeFname("Ataur");
-		Thread.sleep(4000);
-	}
-	@Test(priority=2)
+	
+	@Test(priority=3)
 	public void clickOndownload() throws InterruptedException {
 		test=report.createTest("SuperTech");
 		SuperTechHomePage suhp=PageFactory.initElements(driver, SuperTechHomePage.class);
